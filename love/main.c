c61a37bd342e29fa4b416effb426ac982828d956
brandon82890 <brandon82890@gmail.com>
2022-02-11 16:57:27 -0500

    add parsechar and love

---
+++ love/main.c
@@ -0,0 +1,19 @@
+/* Love you how much
+ * Brandon Johnson
+ * February 11, 2022
+ */
+
+#include <stdio.h>
+
+int main(void) {
+  int repeat = 0;
+  printf("How strong is your love 1-10? : ");
+  scanf("%d", &repeat);
+  printf("\nI love you very");
+  while (repeat > 0) {
+    printf("\n very");
+    repeat--;
+  };
+  printf(" much.\n\n");
+  return 0;
+};
